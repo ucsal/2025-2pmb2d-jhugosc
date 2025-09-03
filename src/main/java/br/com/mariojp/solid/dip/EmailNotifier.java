04901e3cf740cbe809db09bc1f7a958ebe0f0987
jhugosc <sjoaohugo@gmail.com>
2025-09-03 17:49:54 -0300

    refactor: EmailNotifier deixa de instanciar SmtpClient e começa a utilizar MailSender

--- src/main/java/br/com/mariojp/solid/dip/EmailNotifier.java
+++ src/main/java/br/com/mariojp/solid/dip/EmailNotifier.java
@@ -1,12 +1,16 @@
 package br.com.mariojp.solid.dip;
 
 public class EmailNotifier {
-	private final SmtpClient smtp = new SmtpClient();
+	private final MailSender mailSender;
 
-	public void welcome(User user) {
+	public EmailNotifier() {
 		if ("true".equalsIgnoreCase(System.getProperty("DRY_RUN"))) {
-			// Estado inicial: ainda assim usa SMTP real (bug proposital)
-		}
-		smtp.send(user.email(), "Bem-vindo", "Olá " + user.name());
+			this.mailSender = new NoopMailSender();
+		} else {
+            this.mailSender = new SmtpMailSender();
+        }
+    }
+	public void welcome (User user) {
+		this.mailSender.send(user.email(), "Bem-vindo", "Olá " + user.name());
 	}
 }
